b46928d627955cffa10c16c97b0914cfd1f8ae34
Aleksander <ano@dixa.com>
2021-04-27 20:05:54 +0200

    Make regen script publish changes

--- dixatestsystem/regenerate_services.sh
+++ dixatestsystem/regenerate_services.sh
@@ -20,10 +20,17 @@ generate_service () {
       -o "ts-api/src/${DIR_NAME}"
 }
 
+npm_token=$1
+if [ -z "$1" ]; then
+    echo "please provide valid npm token"
+    echo "usage ./regenerate_services.sh [npm_token]"
+    exit 1
+fi
+
 services=( 
    "conversation-service"
-   "message-service"
-   "queue-service"
+   # "message-service"
+   # "queue-service"
    # "offer-service"
    # "user-service"
 )
@@ -34,6 +41,27 @@ do
    echo "REGENERATING $i"
    echo ""
    generate_service $i > /dev/null
+done
+
+echo ""
+echo "UPDATING CLIENT PACKAGE"
+echo ""
+
+cd "ts-api"
+npm version minor
+npm publish
+cd ..
+
+for i in "${services[@]}"
+do
+   echo ""
+   echo "UPGRADING CLIENT LIBS: $i"
+   echo ""
+   cd "${i}"
+   npm update
+   npm audit fix
+   cd "deploy"
+   AWS_PROFILE=security bash deploy.sh ${npm_token}
 done
 
 echo ""
